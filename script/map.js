312bda3b6f2aed5ef6e8bcb81a45047bd9e0dd34
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2017-11-14 14:35:44 -0600

    adding location script for mnt,bg

--- script/map.js
+++ script/map.js
@@ -1,1 +1,11 @@
-
+function initMap() {
+    var location1 = {lat: 43.414693, lng: 23.228296};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 5,
+      center: location1
+    });
+    var marker1 = new google.maps.Marker({
+      position: location1,
+      map: map
+    });
+}
