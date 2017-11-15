a60797765b117650bc8409c1be26e739551c6bbb
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2017-11-14 23:13:51 -0600

    Update map.js

--- script/map.js
+++ script/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var location1 = {lat: 43.414693, lng: 23.228296};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 33,
+      zoom: 23,
       mapTypeId: 'satellite',
       center: location1
     });
